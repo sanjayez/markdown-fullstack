d9506021dc7018acb735f77677cf74f4c619a145
Sanjay <sanu2336@gmail.com>
2023-03-16 20:15:50 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-blue-600">Hello!</h1>
+    <div className="my-4">
+      <Routes>
+        <Route path='/' element={ <h1>Hi</h1> } />
+        <Route path='/new' element={ <h1>New</h1> } />
+        <Route path='/:id'>
+          <Route index element={<h1>Show</h1>}/>
+          <Route path='edit' element={<h1>Edit</h1>}/>
+        </Route>
+        <Route path='*' element={ <h1>Not found.</h1> } />
+      </Routes>
     </div>
   )
 }
